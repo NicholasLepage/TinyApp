36655e70c50def24cfa74000b29302fc5ab18c4f
Nicholas Lepage <nic.lep@hotmail.com>
2018-06-12 20:57:35 +0000

    Finished setting up the server and added the few starting pages, as well as linking them together

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,6 +21,18 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+                        longURL: urlDatabase[req.params.id]
+                       };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
