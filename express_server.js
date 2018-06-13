d631361c2fb80d9e6023be80f5466a66318e0731
Nicholas Lepage <nic.lep@hotmail.com>
2018-06-13 16:15:20 +0000

    Added delete function + added links to header

--- express_server.js
+++ express_server.js
@@ -3,12 +3,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -28,12 +31,44 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls/:id/delete", (req,res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls/");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
                         longURL: urlDatabase[req.params.id]
                        };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  var newLongURL = req.body["longURL"];
+  var newShortURL = generateRandomString();
+  console.log(req.body);  // debug statement to see POST parameters
+  res.redirect("/urls/" + newShortURL);
+  urlDatabase[newShortURL] = newLongURL;         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = req.params["shortURL"];
+  res.redirect(urlDatabase[longURL]);
+});
+
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
